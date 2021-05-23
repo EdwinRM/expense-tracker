ad32eb14617f07ee9cc1c01a106bb096cfdc68de
edwin <edwinramirez@edwins-mac-mini.attlocal.net>
2021-05-23 10:21:06 -0500

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import "./ExpenseItem.css";
+import React, {useState}  from "react";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  const [value, setTitle] = useState(props.title);
+
+  const clickedHandler = () => {
+    setTitle("Updated");
+  };
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{value}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickedHandler}>Change value </button>
+    </div>
+  );
+};
+
+export default ExpenseItem;
